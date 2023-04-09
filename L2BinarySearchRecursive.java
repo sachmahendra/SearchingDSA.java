c7a1d92b11d482b28b224b01499b61953014f0ff
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-04-09 08:28:10 +0530

    initial.commit
    IT IS A JAVA DSA RECURSIVE CODE FOR BINARY SEARCH PROBLEM

---
+++ L2BinarySearchRecursive.java
@@ -0,0 +1,24 @@
+public class L2BinarySearchRecursive {
+    static int bSearch(int arr[], int x,int low,int high){
+        if(low>high) return -1;
+        int mid = (low + high )/2;
+        if(mid==x)
+        return mid;
+        else if(mid>x)
+        bSearch(arr, x, low, high = mid-1);
+        else
+        bSearch(arr, x, low = mid+1, high);
+
+        return mid;
+        
+        
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int x=3;
+        int low=0;
+        int n=5;
+        int high = n-1;
+        System.out.println(bSearch(arr, x, low, high));
+    }
+}
