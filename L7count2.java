be4d65a4057721e846abb9f4cc6849f703875d81
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-04-11 23:55:21 +0530

    initial.commit
    it is a java dsa code for count same as lecture 7

---
+++ L7count2.java
@@ -0,0 +1,26 @@
+public class L7count2{
+    static int countOne(int arr[]){
+        int n= arr.length;
+        int low = 0; int high = n-1;
+        while(low<= high){
+            int mid= (low+high)/2;
+            if(arr[mid]==0)
+            low= mid+1;
+
+            else
+            {
+                if(mid==0 || arr[mid -1 ]==0)
+                return (n-mid);
+                else
+                high = mid-1;
+            }
+
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int arr[]={0,0,0,1,1,1,1,1};
+        int n=7;
+        System.out.println(countOne(arr));
+    }
+}
