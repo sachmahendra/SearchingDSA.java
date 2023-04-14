06b516099c66be3bb4e4170bb2d982e8fcc96a61
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-04-14 08:00:16 +0530

    initial.commit
    IT IS A JAVA DSA CODE TO FIND OUT MAJORITY ELEMENT IN AN ARRAY

---
+++ L14MajorityElement.java
@@ -0,0 +1,17 @@
+public class L14MajorityElement {
+    static int majorEle(int arr[], int n){
+        int count =1;
+        for(int i=1; i<n;i++){
+            if(arr[i]==arr[i-1])
+            count++;
+            if(count++ > n/2)
+            return arr[i];
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]= {6,8,4,8,8};
+        int n = 5;
+        System.out.println(majorEle(arr, n));
+    }
+}
